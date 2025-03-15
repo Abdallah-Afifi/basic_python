1a0ca67287559c90b0c564b9a8b9dfc601aae44f
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 21:45:12 -0400

    adding fn arg to run func

--- basic.py
+++ basic.py
@@ -129,7 +129,7 @@ class Lexer:
         
 
 # Run function
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     return tokens, error
