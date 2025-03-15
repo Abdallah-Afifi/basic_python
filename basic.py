3bc6d42e627f3b0477c4b78878a62f6e91fe9c0c
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 22:40:58 -0400

    integrate positions into tokens

--- basic.py
+++ basic.py
@@ -67,9 +67,16 @@ class Position:
 # Token class
 
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type_ = type_
         self.value = value
+
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
+        if pos_end:
+            self.pos_end = pos_end.copy()
     
     def __repr__(self):
         if self.value:
