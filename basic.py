245b13c4cd9f587ac4109bc4b7c7337b1e4f116c
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 21:38:30 -0400

    generic position class

--- basic.py
+++ basic.py
@@ -26,6 +26,27 @@ class IllegalCharError(Error):
         super().__init__('Illegal Character', details)
 
 
+# Position class
+class Position:
+    def __init__(self, idx, ln, col):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+    
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+        
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col)
+
+
 # Token class
 
 class Token:
