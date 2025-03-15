559de9ec11356072f276e4fe38e17ca4a6b7cf40
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 22:01:41 -0400

    initial parse func

--- basic.py
+++ basic.py
@@ -160,6 +160,10 @@ class Parser:
             self.current_tok = self.tokens[self.tok_idx]
         return self.current_tok
     
+    def parse(self):
+        res = self.expr()
+        return res
+    
 
 
 
