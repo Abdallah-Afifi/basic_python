dcb207f95d79001784ab52c04148ab1ca5d40480
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 21:20:36 -0400

    lexer class implementation

--- basic.py
+++ basic.py
@@ -21,3 +21,16 @@ class Token:
         if self.value:
             return f'{self.type_}:{self.value}'
         return f'{self.type_}'
+    
+# Lexer class
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
